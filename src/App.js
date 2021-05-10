7f7899f0de746c04a4b5a1455a1ad3e57734dd19
qoralswn82 <58283656+qoralswn82@users.noreply.github.com>
2021-05-10 22:13:14 +0900

    Revert "코드 수정 : 주석 추가"
    This reverts commit b42fce0f4b48ba67d18f73feace366075d18801e.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import Layout from "./layouts/Layout";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Route, Switch } from "react-router-dom";
 import dashBoard from "./routes/Dashboard";
 import Main from "./routes/Main";
 export default function App() {
@@ -8,10 +8,8 @@ export default function App() {
     // <div className="App">
     <BrowserRouter>
       <Layout>
-        {/*test Route */}
         <Route path="/dashBoard" exact={true} component={dashBoard} />
         <Route path="/" exact={true} component={Main} />
-        {/* Route 작성 */}
       </Layout>
     </BrowserRouter>
     // </div>

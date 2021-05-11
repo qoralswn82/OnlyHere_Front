a4c20a52d1e2dcf919c0509b3775e773aee9322e
baekminju <bmj@jkds.kr>
2021-05-11 18:21:56 +0900

    기능 추가 : 회원가입 아이디 중복 체크 함수 회원가입 validation check 추가 회원가입 함수 + 경로이동 추가

--- src/routes/Login/Join.js
+++ src/routes/Login/Join.js
@@ -1,8 +1,98 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// import counter from 'easytimer';
+// import useTimer from 'easytimer-react-hook';
+
 
 class Join extends React.Component {
     state={
-        dupId : false
+        dupId : false,
+        preCertification : false,
+        certification : false,
+        count: null,
+        id : null,
+        pw : null,
+        name : null,
+        nickNm : null,
+        email : null,
+        telNo : null
+    }
+
+    dupId = _=>{
+        this.setState({
+            dupId : true
+        })
+    }
+    
+    preCertification=_=>{
+        //인증번호 발송 및 이메일로 전송
+        //this.state.count 카운트 하기
+        // const timer = new Timer();
+        // timer.start({countdown: true, startValues: {seconds: 300}});
+        // this.setState({
+        //     count : timer.getTimeValues().toString()
+        // })
+
+        // timer.addEventListener('secondsUpdated', function (e) {
+        //     const value =  timer.getTimeValues().toString();
+        //     this.setState({
+        //         count : value
+        //     })
+        // });
+        
+        // timer.addEventListener('targetAchieved', function (e) {
+        //     this.setState({
+        //         count : timer.getTimeValues().toString()
+        //     })
+        // });
+
+        //easytimer -react- hook
+        // const [timer, isTargetAchieved] = useTimer({
+        //     /* Hook configuration */
+        //     timeValues: counter
+           
+        // });
+    
+        // timer.start({
+        //     /* EasyTimer start configuration */
+        //     startValues : {seconds: 300},
+        //     countdown : true
+        // });
+        //성공 시
+        this.setState({
+            preCertification : true
+        })
+        //실패 시 -> x 
+    }
+
+    getCertification=(e)=>{
+        
+        //발송한 인증번호와 비교 후 
+        this.setState({
+            certification : true
+        })
+        alert("인증이 완료 되었습니다.");
+    }
+
+    validationJoin(){
+        if(!this.state.dupId){
+            alert("아이디 중복 체크를 해주세요.");
+            return false;
+        }
+        if(!this.state.certification){
+            alert("인증을 진행해 해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    join=_=>{
+        if(!this.validationJoin()){
+            return;
+        }
+        //회원가입 db
+        const {history} = this.props;
+        history.push("/joinComp");
     }
     render(){
         return(<div>
@@ -17,7 +107,7 @@ class Join extends React.Component {
             <div>
                 <label>아이디</label>
                 <input type="text"/>
-                <button>중복확인</button>
+                <button onClick={this.dupId}>중복확인</button>
                 {this.state.dupId ? <label>아이디 중복</label> : <label></label>}
                 <label>비밀번호</label>
                 <input type="password"/>
@@ -31,9 +121,15 @@ class Join extends React.Component {
                 <input type="email"/>
                 <label>전화번호</label>
                 <input type="text"/>
-                <input type="text" placeholder="인증번호를 입력하세요."/>
-                <button>취소</button>
-                <button>확인</button>
+                <button onClick={this.preCertification}>인증번호 받기</button>
+                { this.state.preCertification &&
+                <div>                
+                    <input type="text" onChange={(e)=>this.getCertification(e)} placeholder="인증번호를 입력하세요."/>
+                    <label>{this.state.count}</label>
+                </div>
+                }
+                {/** 취소 없이 상단 로고를 눌러 자동 취소 */}
+                <button onClick={this.join}>가입하기</button>
             </div>
         </div>)
     };

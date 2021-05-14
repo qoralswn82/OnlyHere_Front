215427185bdf7cfea070cde18e639e13794d0ff1
baekminju <bmj@jkds.kr>
2021-05-14 17:13:13 +0900

    기능 추가 : 로그인( input 태그 value 공통함수 적용 테스트) - input 태그 value 값 state 에 할당하는 공통함수 적용 테스트

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -3,21 +3,28 @@ import { Link } from "react-router-dom";
 
 class Login extends React.Component{
     state = {
-        id : null,
-        pw : null
+        
+            id : null,
+            pw : null
+        
     }
 
-    getId = (e)=>{
-        this.setState({
-            id : e.target.value
-        })
-    }
+    // getId = (e)=>{
+    //     this.setState({
+    //         id : e.target.value
+    //     })
+    // }
 
-    getPw = (e)=>{
-        this.setState({
-            pw : e.target.value
-        })
-    }
+    // getPw = (e)=>{
+    //     this.setState({
+    //         pw : e.target.value
+    //     })
+    // }
+        inputValue =(e)=>{
+            this.setState({
+                [e.target.name] : e.target.value
+            })
+        }
 
     Login=()=>{
         //1. id,pw db 저장
@@ -27,16 +34,16 @@ class Login extends React.Component{
     render(){
         return (
             <div>
-                <input type="text" name="id"  onChange={(e)=>this.getId(e)} placeholder="아이디"/>
-                <input type="password" name="pw"  onChange={(e)=>this.getPw(e)} placeholder="비밀번호"/>
+                <input type="text" name="id"  onBlur={(e)=>{this.inputValue(e)}} placeholder="아이디"/>
+                <input type="password" name="pw"  onBlur={(e)=>{this.inputValue(e)}} placeholder="비밀번호"/>
                 <h1>ID : {this.state.id}</h1>
                 <h1>PW : {this.state.pw}</h1>
                 <button onClick={this.Login()}>로그인</button>
-                <Link to="/terms"><button>회원가입</button></Link>
-                <Link to="/findId">
+                <Link to="/login/terms"><button>회원가입</button></Link>
+                <Link to="/login/findId">
                     <a>아이디 찾기</a>
                 </Link>
-                <Link to="/findPw">
+                <Link to="/login/findPw">
                 <a>비밀번호 찾기</a>
                 </Link>
             </div>

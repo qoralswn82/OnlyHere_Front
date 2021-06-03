bdd11ce96161135e623e8875cfdd875d3795665c
baekminju <bmj@jkds.kr>
2021-06-03 17:35:11 +0900

    주석 추가

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
   return (
     // <div className="App">
     <BrowserRouter>
+      {/* path : '/login*'일 경우와 일반적인 화면 layout 파일 안에서 분기*/}
       <Layout>
         {/*test Route */}
         <Route path="/dashBoard" exact={true} component={dashBoard} />
@@ -24,6 +25,7 @@ export default function App() {
         <Route path="/login/terms" exact={true} component={Terms} /> 
         <Route path="/login/join" exact={true} component={Join} /> 
         <Route path="/login/joinComp" exact={true} component={JoinComp} /> 
+        {/* 같은 파일 link path에 따른 분기 */}
         <Route path="/login/findId" exact={true} component={Find} /> 
         <Route path="/login/findPw" exact={true} component={Find} /> 
       </Layout>

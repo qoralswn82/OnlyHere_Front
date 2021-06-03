17006f7db3dc9a47465db40c2ab11c00a8edfa5f
baekminju <bmj@jkds.kr>
2021-06-03 17:36:46 +0900

    layout  분기 : path에 따른 분기 > login의 경우 header . menu. footer 제거 > 그 외의 경우 header . menu. footer 적용

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, {Fragment} from "react";
 import Header from "./Header";
 import Menu from "./Menu";
 import Footer from "./Footer";
 
 class Layout extends React.Component {
   state = {
-    type: "ind" //
-  };
+    type: "ind", //
+    path : (window.location.pathname).slice(0,6) 
+  }
+  componentDidMount(){
+    console.log(window.location.pathname);
+    console.log(this.props.children);
+    console.log(this.props);
+   
+  }
   render() {
     return (
-      <div class="container">
-        <Header />
-        <Menu type={this.state.type} />
-        <div>{this.props.children}</div>
-        <Footer />
-      </div>
+      <Fragment>
+        {this.state.path === '/login' ?  
+        <div>{this.props.children}</div>: 
+        <div class="container">
+          <Header />
+          <Menu type={this.state.type} />
+          <div>{this.props.children}</div>
+          <Footer />
+          </div>
+        }
+      </Fragment>
     );
   }
 }
